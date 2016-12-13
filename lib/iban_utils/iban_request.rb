513913a7adb585d92b5a0172a68d2efc78a6e7f9
Stanimir Stamenkov <stanimir.stamenkov@sumup.com>
2016-12-13 15:34:06 +0200

    [SRP-164]  Reuse persistent connection for multiple requests

--- lib/iban_utils/iban_request.rb
+++ lib/iban_utils/iban_request.rb
@@ -1,5 +1,6 @@
 require 'digest'
 require 'uri'
+require 'net/http'
 
 class IbanRequest
   def initialize(config)
@@ -9,8 +10,7 @@ class IbanRequest
   end
 
   def submit(params)
-    http = Net::HTTP.new(@uri.host, @uri.port)
-    http.use_ssl = true if @uri.scheme == 'https'
+    http = IbanRequest.http(@uri)
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE unless @ssl_verify
 
     @request = Net::HTTP::Post.new(@uri.request_uri)
@@ -33,5 +33,34 @@ class IbanRequest
     params
   end
   private :hash_password
+
+  # @param   uri [URI]  endpoint address the HTTP client should connect to
+  # @return  [Net::HTTP]  HTTP client to use for connecting to the given URL.
+  #   If the host or port of any previously initialized client differ from
+  #   the given URL, the previous session will be finished and new one
+  #   started.
+  def self.http(uri)
+    client = @@http_client
+    return client if same_endpoint?(client, uri)
+
+    client && client.finish rescue nil
+
+    client = Net::HTTP.new(uri.host, uri.port)
+    client.use_ssl = true if uri.scheme == 'https'
+    client.keep_alive_timeout = 15
+    client.start
+    @@http_client = client
+  end
+
+  def self.same_endpoint?(client, uri)
+    return false unless client
+
+    client.address == uri.host &&
+        client.port == uri.port &&
+        client.use_ssl? == (uri.scheme == 'https')
+  end
+  private_class_method :same_endpoint?
+
+  @@http_client = nil
 
 end
