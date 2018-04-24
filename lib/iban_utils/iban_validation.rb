39471e2716703df7e17e94c4f93d03975796880b
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2018-04-24 16:51:34 +0300

    [SRP-413] Add method comment showing sample reIBAN response

--- lib/iban_utils/iban_validation.rb
+++ lib/iban_utils/iban_validation.rb
@@ -16,6 +16,26 @@ class IbanValidation
     @response.passed?
   end
 
+  #
+  # @raise [EmptyIbanError]
+  #
+  # @return [IbanResponse]
+  # Example:
+  # <IbanResponse:0x007fb29c9f0c80
+  # @comment=
+  #   "Iban generation returned with code: 0.\nResult: passed\nResult can be assumed correct. \nRESPONSE Details:\n
+  #   Status: passed\n  Return Code: 0\n  Bank: Commerzbank\n  Bank Address: 50447 Köln\n  BIC Candidates: COBADEFFXXX",
+  # @details=
+  #   {:bank=>"Commerzbank",
+  #    :bank_address=>"50447 Köln",
+  #    :iban=>"DE89370400440532013000",
+  #    :account_number=>"0532013000",
+  #    :bank_code=>"37040044",
+  #    :possible_bics=>["COBADEFFXXX"],
+  #    :swift=>"COBADEFFXXX"},
+  # @raw=#<Net::HTTPOK 200 OK readbody=true>, @return_code=0, @return_message="passed">
+  # >
+  #
   def validate_and_get_info(iban)
     # no need to send a validation request for an obviously invalid ibans
     unless iban && iban.is_a?(String) && !iban.gsub(/\s/, '').empty?
